e9eacc8e6c7826e2bf74a10bb1ceac08343ab552
unknown <kim0615157@gmail.com>
2025-11-15 22:52:50 +0900

    feat:우승자를 결정
    거리가 최대인 자동차를 찾음
    
    동률이면 우승자를 여러명으로 둠

--- lib/models/racing_game.dart
+++ lib/models/racing_game.dart
@@ -4,6 +4,7 @@ class RacingGame {
   final List<Car> cars;
   int currentTurn = 0;
   int totalTurns = 0;
+  List<String> winners = [];
 
   RacingGame({required List<String> carNames}):
     cars = carNames.map((name) => Car(name)).toList();
@@ -13,5 +14,36 @@ class RacingGame {
       car.move();
     }
     currentTurn += 1;
+  }
+
+  int findMaxDistance(List<Car> cars) {
+    int maxDistance = 0;
+
+    for (var car in cars) {
+      if (car.distance > maxDistance) {
+        maxDistance = car.distance;
+      }
+    }
+    return maxDistance;
+  }
+
+  List<String> findWinners(List<Car> cars, int maxDistance) {
+    List<String> winners = [];
+
+    for (var car in cars) {
+      if (car.distance == maxDistance) {
+        winners.add(car.name);
+      }
+    }
+    return winners;
+  }
+
+  void determineWinners() {
+    if (cars.isEmpty) {
+      return;
+    }
+
+    int maxDistance = findMaxDistance(cars);
+    winners = findWinners(cars, maxDistance);
   }
 }
