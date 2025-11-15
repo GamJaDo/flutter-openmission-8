ab7d323a0d71813ba15a673a6b0e4177ecc87a35
unknown <kim0615157@gmail.com>
2025-11-15 22:13:31 +0900

    feat: 자동차 한 턴을 진행

--- lib/models/racing_game.dart
+++ lib/models/racing_game.dart
@@ -2,8 +2,16 @@ import 'package:flutter_openmission_8_racingcar/models/car.dart';
 
 class RacingGame {
   final List<Car> cars;
+  int currentTurn = 0;
+  int totalTurns = 0;
 
   RacingGame({required List<String> carNames}):
     cars = carNames.map((name) => Car(name)).toList();
 
+  void playTurn() {
+    for (var car in cars) {
+      car.move();
+    }
+    currentTurn += 1;
+  }
 }
