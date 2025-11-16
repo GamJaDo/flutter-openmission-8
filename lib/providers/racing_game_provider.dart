c7708f68bb2ce4e4d9e8e6bafcf1b90ebe589b24
unknown <kim0615157@gmail.com>
2025-11-16 18:38:18 +0900

    feat:자동차 이름과 이동 횟수로 게임 시작

---
+++ lib/providers/racing_game_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_openmission_8_racingcar/models/racing_game.dart';
+
+class RacingGameProvider extends ChangeNotifier {
+  RacingGame? _game;
+
+  RacingGame? get game {
+    return _game;
+  }
+
+  bool get isGameStarted {
+    return _game != null;
+  }
+
+  int get currentTurn {
+    if (_game == null) {
+      return 0;
+    }
+    return _game!.currentTurn;
+  }
+
+  int get totalTurns {
+    if (_game == null) {
+      return 0;
+    }
+    return _game!.totalTurns;
+  }
+
+  void startGame(List<String> carNames, int moveCount) {
+    _game = RacingGame(carNames: carNames);
+    _game!.totalTurns = moveCount;
+    notifyListeners();
+  }
+}
