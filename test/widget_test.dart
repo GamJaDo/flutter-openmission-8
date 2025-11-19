b3854a39f6868db4eaa73bd60050d15202dc5f7b
unknown <kim0615157@gmail.com>
2025-11-19 11:19:54 +0900

    refacor:애플리케이션 이름 변경
    애플리케이션 icon 변경

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application_2/main.dart';
+import 'package:flutter_openmission_8_racingcar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const RacingCarApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
